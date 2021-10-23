ab4fd51bc22f503bc5315fc1c08fa1601b56ed80
Kshitij Kumar <kshitijkumar230@gmail.com>
2021-10-23 09:55:40 +0530

    Add files via upload

---
+++ 05AssignmentOperators.py
@@ -0,0 +1,22 @@
+
+print("Assignment Operators:")  # Used for assigning
+# All Assignment Operators: +, -,* , /, //, **, %
+
+x = 5  # assigning 5 to x i.e the value of x is now 5
+print(x)
+x += 4  # modifying the value of x by adding 4 i.e x = 5 + 4
+print(x)
+x -= 4  # modifying the value of x by subtracting 4 i.e x = 9 - 4
+print(x)
+x *= 4  # modifying the value of x by multiplying 4 i.e x = 5 * 4
+print(x)
+x /= 4  # modifying the value of x by dividing it by 4 i.e x = 20 / 4
+print(x)
+x %= 4  # modifying the value of x by dividing it by 4 and will return only remainder i.e x = 5 / 4
+print(x)
+x = 15
+x //= 4  # modifying the value of x by dividing it by 4 and will return only integer value i.e x = 15 // 4
+print(x)
+x **= 3  # modifying the value of x by raising it to the power 3
+# i.e x = 3 ** 3
+print(x)
