46aafb7b23692afc00c5dd39ebd55001e9f689ac
Kshitij Kumar <kshitijkumar230@gmail.com>
2021-10-23 09:54:27 +0530

    Add files via upload

---
+++ 03operators.py
@@ -0,0 +1,11 @@
+print("""
+Operators in Python:
+1. Arithmetic Operators
+2. Assignment Operators
+3. Comparison Operators
+4. Logical Operators
+5. Identity Operators
+6. Membership Operators
+7. Bitwise Operators
+We will look at each of the operator one by one in further practicals/programs
+""")
