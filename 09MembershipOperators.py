fecd59ab4543265fbf92d167a49a474130e7c2b3
Kshitij Kumar <kshitijkumar230@gmail.com>
2021-10-23 09:57:34 +0530

    Add files via upload

---
+++ 09MembershipOperators.py
@@ -0,0 +1,7 @@
+
+print("Membership Operators")
+# All Membership operators: in, not in
+list1 = [3, 3, 2, 2, 39, 33, 35, 32]
+print(32 in list1)  # will return True
+print(332 in list1)  # will return False
+print(332 not in list1)  # will return True
