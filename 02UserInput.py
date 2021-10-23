29f85cb83f747a5471d269e4263e7d21e824f07a
Kshitij Kumar <kshitijkumar230@gmail.com>
2021-10-23 09:52:44 +0530

    Add files via upload

---
+++ 02UserInput.py
@@ -0,0 +1,17 @@
+## To interact efficiently we need to take user input
+## In python we take input from user by using a builtin function called "input()"
+## Same as used below...
+
+userInput = input()
+print(userInput)
+
+## We can also give placeholders in the Input same as below
+userInput2 = input("How are you? ")
+print(userInput2)
+
+## Practice
+user_input = input()
+print(user_input)
+
+user_input2 = input("Write your name here: ")
+print("Hello,", user_input2)
