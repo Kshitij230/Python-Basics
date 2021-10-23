ac4f6875b891930cdffd0c72ad0d2f9c93cb3caf
Kshitij Kumar <kshitijkumar230@gmail.com>
2021-10-23 09:45:18 +0530

    Add files via upload

---
+++ 01print.py
@@ -0,0 +1,17 @@
+## In this program we will see that how can we print anything through Python!!
+
+## We just have to use a builtin function called "print()" to print anything on the terminal
+## Let us try...
+
+print("Hello World")  ## We can print a full sentence
+
+print(14)  ## We can print a number
+
+a = 2
+print(a)  ## We can print a variable assign to a number
+
+b = "Hey"
+print(b)  ## We can print a variable assigned to a word or sentence
+
+## What if we use blank print statement?
+print()  ## It will generate a new line!!
