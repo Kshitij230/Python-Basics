4efd43b3872b3366a7614879a2a469bcc0fffb9f
Kshitij Kumar <kshitijkumar230@gmail.com>
2021-10-23 09:56:17 +0530

    Add files via upload

---
+++ 06ComparisonOperators.py
@@ -0,0 +1,10 @@
+
+print("Comparison Operators:")
+### All operators: ==, !=, <, >, <=, >=
+i = 8
+print(i == 8)  # Comparing value of i (will return True)
+print(i != 8)  # will return False
+print(i > 6)  # will return True
+print(i < 8)  # will return False
+print(i <= 8)  # will return True
+print(i >= 10)  # will return False
