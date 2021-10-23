606f659af2f0dbc902609a7ea797dddab4c22803
Kshitij Kumar <kshitijkumar230@gmail.com>
2021-10-23 09:56:52 +0530

    Add files via upload

---
+++ 07LogicalOperators.py
@@ -0,0 +1,16 @@
+print("Logical Operators")
+# All logical operators = "or operator" and "and operator"
+
+
+"""
+True and True = True
+True and False = False
+False and False = False
+False and True = False
+"""
+
+a = True
+b = False
+print(b and a)
+# If any one situation is True it will return True in case of 'or' operator
+print(b or a)
