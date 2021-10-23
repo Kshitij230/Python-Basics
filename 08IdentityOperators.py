008af6af4368e3e70d7d2690ac8071f926febc2e
Kshitij Kumar <kshitijkumar230@gmail.com>
2021-10-23 09:57:14 +0530

    Add files via upload

---
+++ 08IdentityOperators.py
@@ -0,0 +1,10 @@
+a = True
+b = False
+
+print("Identity Operators:")
+# All identity operators: is, is not
+print(b is b)  # will return True
+print(a is b)  # will return False
+print(5 is not 7)  # will return True
+print(5 is not 5)  # will return False
+print(a is not b)  # will return True
