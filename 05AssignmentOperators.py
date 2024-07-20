139641729d88a85c6d56a60f0ceca941cd8172f7
Kshitij Kumar <kshitijkumar230@gmail.com>
2024-07-20 22:25:33 +0530

    Update 05AssignmentOperators.py

--- 05AssignmentOperators.py
+++ 05AssignmentOperators.py
@@ -1,22 +1,21 @@
 
 print("Assignment Operators:")  # Used for assigning
-# All Assignment Operators: +, -,* , /, //, **, %
+# All Assignment Operators: =, +, -, *, /, //, **, %
 
 x = 5  # assigning 5 to x i.e the value of x is now 5
 print(x)
-x += 4  # modifying the value of x by adding 4 i.e x = 5 + 4
+x += 4  # modifying the value of x by adding 4 i.e x = 5 + 4 = 9
 print(x)
-x -= 4  # modifying the value of x by subtracting 4 i.e x = 9 - 4
+x -= 4  # modifying the value of x by subtracting 4 i.e x = 9 - 4 = 5
 print(x)
-x *= 4  # modifying the value of x by multiplying 4 i.e x = 5 * 4
+x *= 4  # modifying the value of x by multiplying 4 i.e x = 5 * 4 = 20
 print(x)
-x /= 4  # modifying the value of x by dividing it by 4 i.e x = 20 / 4
+x /= 4  # modifying the value of x by dividing it by 4 i.e x = 20 / 4 = 5
 print(x)
-x %= 4  # modifying the value of x by dividing it by 4 and will return only remainder i.e x = 5 / 4
+x %= 4  # modifying the value of x by dividing it by 4 and assigning remainder to it i.e x = 5 % 4 = 1
 print(x)
 x = 15
-x //= 4  # modifying the value of x by dividing it by 4 and will return only integer value i.e x = 15 // 4
+x //= 4  # modifying the value of x by dividing it by 4 and assigning only the integer value before decimal i.e x = 15 // 4 = 3
 print(x)
-x **= 3  # modifying the value of x by raising it to the power 3
-# i.e x = 3 ** 3
+x **= 3  # modifying the value of x by raising it to the power 3 i.e x = 3 ** 3 = 27
 print(x)
