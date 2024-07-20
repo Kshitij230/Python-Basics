e2e73d188c4737b3607ee85131137ff691b700ad
Kshitij Kumar <kshitijkumar230@gmail.com>
2024-07-20 22:21:03 +0530

    Update 04ArithmaticOperators.py

--- 04ArithmaticOperators.py
+++ 04ArithmaticOperators.py
@@ -1,10 +1,10 @@
 
-print("Arithmetic Operators:")  # Used fo Numerical calculations
+print("Arithmetic Operators:")  # Used for Numerical calculations
 
-print("5 + 6 is", 5 + 6)  # Add
+print("5 + 6 is", 5 + 6)  # Adds
 print("5 - 6 is", 5 - 6)  # Subtracts
 print("5 * 6 is", 5 * 6)  # Multiplies
-print("5 ** 3 is", 5 ** 3)  # 5 to the power 3 i.e 5*5*5
+print("5 ** 3 is", 5 ** 3)  # 5 to the power 3 i.e. 5*5*5
 print("5 / 6 is", 5 / 6)  # Divides
 print("15 // 6 is", 15 // 6)  # Gives integer value (forgot the values after decimal)
 print("13 % 6 is", 13 % 6)  # Gives remainder
